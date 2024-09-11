df22850b365e8a7f770149a2b7d9f0f8a7d4e510
Jewel <nanaaafianyamekye2@gmail.com>
2024-09-11 22:02:14 +0100

    Added forms, admin and signals

--- checkout/models.py
+++ checkout/models.py
@@ -6,7 +6,6 @@ from django.conf import settings
 
 from products.models import Product
 
-# Create your models here.
 class Order(models.Model):
     order_number = models.CharField(max_length=32, null=False, editable=False)
     full_name = models.CharField(max_length=50, null=False, blank=False)
@@ -21,11 +20,11 @@ class Order(models.Model):
     date = models.DateTimeField(auto_now_add=True)
     delivery_cost = models.DecimalField(max_digits=6, decimal_places=2, null=False, default=0)
     order_total = models.DecimalField(max_digits=10, decimal_places=2, null=False, default=0)
-    grand_toal = models.DecimalField(max_digits=10, decimal_places=2, null=False, default=0)
+    grand_total = models.DecimalField(max_digits=10, decimal_places=2, null=False, default=0)
 
     def _generate_order_number(self):
         """
-        Generate a random, unique order number UUID
+        Generate a random, unique order number using UUID
         """
         return uuid.uuid4().hex.upper()
 
@@ -42,9 +41,8 @@ class Order(models.Model):
         self.grand_total = self.order_total + self.delivery_cost
         self.save()
 
-    def save(self, *args, **Kwargs):
+    def save(self, *args, **kwargs):
         """
-
         Override the original save method to set the order number
         if it hasn't been set already.
         """
@@ -57,13 +55,13 @@ class Order(models.Model):
 
 
 class OrderLineItem(models.Model):
-    order = models.ForeignKey(Order, null=False, blank=False, on_delete=models.CASCADE, related_name='lineitem')
+    order = models.ForeignKey(Order, null=False, blank=False, on_delete=models.CASCADE, related_name='lineitems')
     product = models.ForeignKey(Product, null=False, blank=False, on_delete=models.CASCADE)
-    product_size = models.CharField(max_length=2, null=True, blank=True) # XS, S, M, L XL
+    product_size = models.CharField(max_length=2, null=True, blank=True) # XS, S, M, L, XL
     quantity = models.IntegerField(null=False, blank=False, default=0)
     lineitem_total = models.DecimalField(max_digits=6, decimal_places=2, null=False, blank=False, editable=False)
 
-    def save(self, *args, **Kwargs):
+    def save(self, *args, **kwargs):
         """
         Override the original save method to set the lineitem total
         and update the order total.

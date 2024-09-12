8a4af8c2a63d02226e52e3a78884c73a660cff6f
Jewel <nanaaafianyamekye2@gmail.com>
2024-09-12 11:26:27 +0100

    added stripe elements

--- checkout/views.py
+++ checkout/views.py
@@ -9,5 +9,7 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51Py8shP86GD8AZmK2hvqdfhK2Br41mPOih7riU1mY2qQyn6oklWPEEcuDyitl8enkWJh9SgpdMPtcX9VV7mogg7D00drcK3okc',
+        'client_secret': 'test client secret',
     }
     return render(request, template, context)
